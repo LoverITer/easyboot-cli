13debc12a5dc86b1e9e1955788a1c6c38b4b861a
LoverITer <2489868503@qq.com>
2022-01-22 17:38:38 +0800

    fix: build fix

---
+++ easyboot-core/src/main/java/top/easyboot/titan/dao/auto/mapper/DemoMapper.java
@@ -0,0 +1,11 @@
+package top.easyboot.titan.dao.auto.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author: frank.huang
+ * @date: 2022-01-22 17:34
+ */
+@Mapper
+public interface DemoMapper {
+}
