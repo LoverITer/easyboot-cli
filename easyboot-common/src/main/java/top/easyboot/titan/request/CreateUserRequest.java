8babee330df49a5d0025a92bcd0c0cb07b5c8676
LoverITer <2489868503@qq.com>
2022-02-12 22:10:51 +0800

    feat: 添加参数校验和全局参数校验错误处理器

--- easyboot-common/src/main/java/top/easyboot/titan/request/CreateUserRequest.java
+++ easyboot-common/src/main/java/top/easyboot/titan/request/CreateUserRequest.java
@@ -5,8 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 /**
  * Example of request parameter bean to create an object
+ *
  * @author: frank.huang
  * @date: 2021-11-01 20:39
  */
@@ -15,9 +20,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateUserRequest {
-
+    @NotBlank(message = "用户名不能为空")
     private String username;
+
+    @NotBlank
     private String address;
+
+    @Min(value = 0, message = "年龄不能小于0")
+    @Max(value = 200, message = "年龄最大200")
     private Integer age;
 
 }
